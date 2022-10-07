4c6c51a17c0c41ab9f45f22d406561d66564ebd2
Siddharth <siddharthbharmoria@gmail.com>
2022-10-07 09:02:56 +0530

    changed settings forgmail smtp

--- HomeDesign/settings.py
+++ HomeDesign/settings.py
@@ -145,7 +145,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'siddservertest@gmail.com'
-EMAIL_HOST_PASSWORD = 'langna159'
+EMAIL_HOST_PASSWORD = 'emcdvkebtjgbqnib'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
